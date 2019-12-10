3299c4b1af1c0a58cd1d0c22efa275d54f9a508d
Lukas Siemon <lukas.siemon@gmail.com>
2019-12-09 21:50:26 -0800

    feat: basic functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-module.exports = (a, b) => a + b;
+const Joi = require('joi-strict');
+
+module.exports.Pool = (opt = {}) => {
+  Joi.assert(opt, Joi.object().keys({
+    concurrency: Joi.number().integer().min(1).optional()
+  }));
+  const concurrency = opt.concurrency || 10;
+
+  let pending = 0;
+  const queue = [];
+
+  const processQueue = () => {
+    while (queue.length !== 0 && pending < concurrency) {
+      queue.shift()();
+      pending += 1;
+    }
+  };
+
+  return (payload) => {
+    Joi.assert(payload, Joi.alternatives(
+      Joi.function(),
+      Joi.array().items(Joi.function())
+    ));
+
+    const isArray = Array.isArray(payload);
+    const fns = isArray ? payload : [payload];
+
+    return new Promise((resolve, reject) => {
+      const state = {
+        total: fns.length,
+        processed: 0,
+        result: [],
+        success: true
+      };
+      fns.forEach((fn, idx) => {
+        const wrappedFn = () => fn()
+          .catch((e) => {
+            state.success = false;
+            return e;
+          })
+          .then((r) => {
+            state.result[idx] = r;
+            state.processed += 1;
+            pending -= 1;
+            if (state.processed === state.total) {
+              (state.success ? resolve : reject)(isArray ? state.result : state.result[0]);
+            }
+            processQueue();
+          });
+        queue.push(wrappedFn);
+      });
+      processQueue();
+    });
+  };
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,35 @@
+const util = require('util');
 const expect = require('chai').expect;
-const index = require('../src/index');
+const { describe } = require('node-tdd');
+const { Pool } = require('../src/index');
 
-describe('Testing Package', () => {
-  it('Testing Addition', () => {
-    expect(index(7, 9)).to.equal(16);
+const sleep = util.promisify(setTimeout);
+
+describe('Testing Promise Pool', { record: console }, () => {
+  let Worker;
+  before(() => {
+    // eslint-disable-next-line no-console
+    const log = (...args) => console.log(...args);
+    Worker = (id, delayMs) => () => new Promise((resolve) => {
+      log(`start ${id}`);
+      sleep(delayMs);
+      log(`end ${id}`);
+      resolve(id);
+    });
+  });
+
+  it('Testing limit', async ({ recorder }) => {
+    const pool = Pool({ concurrency: 1 });
+    const result = await pool([
+      Worker(1, 200),
+      Worker(2, 100)
+    ]);
+    expect(result).to.deep.equal([1, 2]);
+    expect(recorder.get()).to.deep.equal([
+      'start 1',
+      'end 1',
+      'start 2',
+      'end 2'
+    ]);
   });
 });
